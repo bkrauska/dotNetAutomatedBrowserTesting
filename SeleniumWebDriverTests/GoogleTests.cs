d9cb1b0a4679114a22efd426c9455aaeb3b7f8bd
Brandon Krauska <bkrauska@gmail.com>
2012-11-06 10:01:38 -0600

    Drop the hammer with simple tests for each of the browsers

---
+++ SeleniumWebDriverTests/GoogleTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+
+namespace SeleniumWebDriverTests {
+    [TestClass]
+    public class GoogleTests {
+        [TestMethod]
+        public void open_google_in_ie_64bit() {
+            IWebDriver driver = new OpenQA.Selenium.IE.InternetExplorerDriver(@"..\..\..\web-drivers\IEDriverServer_x64_2.26.0");
+
+            driver.Navigate().GoToUrl("http://google.com/");
+
+            driver.Close();
+        }
+
+        [TestMethod]
+        public void open_google_in_ie_32bit() {
+            IWebDriver driver = new OpenQA.Selenium.IE.InternetExplorerDriver(@"..\..\..\web-drivers\IEDriverServer_Win32_2.26.0");
+
+            driver.Navigate().GoToUrl("http://google.com/");
+
+            driver.Close();
+        }
+
+        [TestMethod]
+        public void open_google_in_chrome() {
+            IWebDriver driver = new OpenQA.Selenium.Chrome.ChromeDriver(@"..\..\..\web-drivers\chromedriver_win_23.0.1240.0");
+
+            driver.Navigate().GoToUrl("http://google.com/");
+
+            driver.Close();
+        }
+
+        [TestMethod]
+        public void open_google_in_firefox() {
+            IWebDriver driver = new OpenQA.Selenium.Firefox.FirefoxDriver();
+
+            driver.Navigate().GoToUrl("http://google.com/");
+
+            driver.Close();
+        }
+    }
+}
